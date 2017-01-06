8c68e8c2ad2a62da0bae8868f4eea30b1b088ed7
Arthur D. Andrade <andradedearthur@gmail.com>
2017-01-06 01:18:39 -0200

    Added page

---
+++ portfolio-page/js/site.js
@@ -0,0 +1,16 @@
+function equalizeCols(clasName) {
+
+    var heights = $(clasName).map(function() {
+        return $(this).height();
+    }).get(),
+
+    maxHeight = Math.max.apply(null, heights);
+
+    $(clasName).height(maxHeight);
+
+}
+
+
+$(document).ready(function(){
+    equalizeCols(".contact-col");   
+});
